5912bc74aaebb677b6eb87768e9ad8e9e6afd1ff
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2023-09-02 17:37:51 +0500

    Added types
    Special thanks to @midgleyc for providing type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,65 @@
 "use strict";
 
-const http = require("http");
-const express = require("express");
-const pify = require("pify");
-const bodyParser = require("body-parser");
+import http, {Server} from "http";
+import { AddressInfo } from "net";
+import express from "express";
+import pify from "pify";
+import bodyParser, { OptionsJson, OptionsText, OptionsUrlencoded } from "body-parser";
 
-const createTestServer = (opts = { bodyParser: true }) => {
-    const server = express();
-    server.http = http.createServer(server);
+// Thank @midgleyc for providing the type definitions
+type ResultServer = TestServer & Omit<express.Express, 'listen'> & {get: (url: string, response: string) => void};
+
+interface Options {
+    /**
+     * Body parser options object to be passed to `body-parser` methods.
+     *
+     * If set to `false` then all body parsing middleware will be disabled.
+     */
+    bodyParser?: false | OptionsJson & OptionsText & OptionsUrlencoded;
+}
+
+interface TestServer {
+    /**
+     * The url you can reach the HTTP server on.
+     *
+     * e.g: `'http://localhost:5486'`
+     *
+     * `undefined` while the server is not listening.
+     */
+    url?: string;
+    /**
+     * The port number you can reach the HTTP server on.
+     *
+     * e.g: `5486`
+     *
+     * `undefined` while the server is not listening.
+     */
+    port?: number;
+    /**
+     * The underlying HTTP server instance.
+     */
+    http: http.Server;
+    /**
+     * Returns a Promise that resolves when both the HTTP and HTTPS servers are listening.
+     *
+     * Once the servers are listening, `server.url` and `server.sslUrl` will be updated.
+     *
+     * Please note, this function doesn't take a port argument, it uses a new randomised port each time.
+     * Also, you don't need to manually call this after creating a server, it will start listening automatically.
+     */
+    listen: () => Promise<void>;
+    /**
+     * Returns a Promise that resolves when both the HTTP and HTTPS servers have stopped listening.
+     *
+     * Once the servers have stopped listening, `server.url` and `server.sslUrl` will be set to undefined.
+     */
+    close: () => Promise<void>;
+}
+
+const createTestServer = (opts: Options = {}): Promise<ResultServer> => {
+    const _express = express();
+    const server = _express as never as ResultServer;
+    server.http = http.createServer(_express);
 
     server.set("etag", false);
 
@@ -35,21 +87,17 @@ const createTestServer = (opts = { bodyParser: true }) => {
         for (const handler of handlers) {
             get(path, send(handler));
         }
-    };
+    } as any;
 
-    server.listen = () => Promise.all([
-        pify(server.http.listen.bind(server.http))().then(() => {
-            server.port = server.http.address().port;
-            server.url = `http://localhost:${server.port}`;
-        }),
-    ]);
+    server.listen = () => pify(server.http.listen.bind(server.http))().then(() => {
+        server.port = (server.http.address() as AddressInfo).port;
+        server.url = `http://localhost:${server.port}`;
+    });
 
-    server.close = () => Promise.all([
-        pify(server.http.close.bind(server.http))().then(() => {
+    server.close = () => pify(server.http.close.bind(server.http))().then(() => {
             server.port = undefined;
             server.url = undefined;
-        }),
-    ]);
+        });
 
     return server.listen().then(() => server);
 };
